dbb05f8db3af70a42204d885175c3c7320b254e4
Guy Maliar <gmaliar@gmail.com>
2013-04-23 11:17:42 +0300

    Update translation_target.rb

--- lib/easy_translate/translation_target.rb
+++ lib/easy_translate/translation_target.rb
@@ -20,7 +20,7 @@ module EasyTranslate
     class TranslationTargetRequest < EasyTranslate::Request
 
       def initialize(target = nil, options = nil)
-        super
+        super(options)
         @target = target
         if options
           @options = options
